abd30a962b79ade1230314d6e8739aae27f9f922
Maria M <marmkhnv@gmail.com>
2023-03-28 17:48:42 +0300

    Rename directories

--- src/main/java/com/practicum/controllers/CatsInteractionController.java
+++ src/main/java/com/practicum/controllers/CatsInteractionController.java
@@ -1,4 +1,4 @@
-package ru.yandex.practicum.controllers;
+package com.practicum.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
